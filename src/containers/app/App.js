e4c4740a63337a229f123b3ddd39f3b17a60c5fd
David <davidbrandon713@gmail.com>
2024-03-28 14:02:18 -0500

    formatting

--- src/containers/app/App.js
+++ src/containers/app/App.js
@@ -6,82 +6,82 @@ import Scroll from '../../components/simple/scroll'
 import '../../index.css'
 
 const App = () => {
-	const userid = 'David'
-	const [searchfield, setSearchfield] = useState('')
-	const [relics, setRelics] = useState([])
+  const userid = 'David'
+  const [searchfield, setSearchfield] = useState('')
+  const [relics, setRelics] = useState([])
   const [iconSize, setIconSize] = useState('small')
 
-	useEffect(() => {
-		onGetRelics()
-	}, [])
+  useEffect(() => {
+    onGetRelics()
+  }, [])
 
-	const onSearchChange = (event) => {
-		setSearchfield(event.target.value)
-	}
+  const onSearchChange = (event) => {
+    setSearchfield(event.target.value)
+  }
 
-	// Get all relic data
-	const onGetRelics = async () => {
-		await fetch(`http://localhost:8080/relics/all/${userid}`, {
-			method: 'GET',
-		})
-			.then((response) => response.json())
-			.then((data) => setRelics(data))
-			.then(console.log('Initialized'))
-	}
+  // Get all relic data
+  const onGetRelics = async () => {
+    await fetch(`http://localhost:8080/relics/all/${userid}`, {
+      method: 'GET',
+    })
+      .then((response) => response.json())
+      .then((data) => setRelics(data))
+      .then(console.log('Initialized'))
+  }
 
-	// Create new relic and user data
-	const onCreateRelic = async (id, name, drops) => {
-		await fetch(`http://localhost:8080/relics/create`, {
-			method: 'POST',
-			headers: { 'Content-Type': 'application/json' },
-			body: JSON.stringify({
-				id,
-				name,
-				drops,
-			}),
-		})
-			.then((response) => response.json())
-			.then((data) => console.log(data))
-			.then(
-				await fetch(`http://localhost:8080/users/${userid}/createrelic/${id}`, {
-					method: 'PATCH',
-					headers: { 'Content-Type': 'application/json' },
-				})
-					.then((response) => response.json())
-					.then((data) => console.log(data))
-			)
-	}
+  // Create new relic and user data
+  const onCreateRelic = async (id, name, drops) => {
+    await fetch(`http://localhost:8080/relics/create`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        id,
+        name,
+        drops,
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => console.log(data))
+      .then(
+        await fetch(`http://localhost:8080/users/${userid}/createrelic/${id}`, {
+          method: 'PATCH',
+          headers: { 'Content-Type': 'application/json' },
+        })
+          .then((response) => response.json())
+          .then((data) => console.log(data))
+      )
+  }
 
-	// Filters relic list by searchfield
-	const filteredRelics = relics.filter((relic) => {
-		let nameMatch = relic.name.toLowerCase().includes(searchfield.toLowerCase())
-		let itemMatch = false
-		for (let i = 0; i < 6; i++) {
-			if (relic.drops[i].toLowerCase().includes(searchfield.toLowerCase())) {
-				itemMatch = true
-			}
-		}
-		return nameMatch || itemMatch
-	})
+  // Filters relic list by searchfield
+  const filteredRelics = relics.filter((relic) => {
+    let nameMatch = relic.name.toLowerCase().includes(searchfield.toLowerCase())
+    let itemMatch = false
+    for (let i = 0; i < 6; i++) {
+      if (relic.drops[i].toLowerCase().includes(searchfield.toLowerCase())) {
+        itemMatch = true
+      }
+    }
+    return nameMatch || itemMatch
+  })
 
-	// Show cards with regard to searchfield
-	return (
-		<div className="app-container">
-			<Header
-				onSearchChange={onSearchChange}
-				onCreateRelic={onCreateRelic}
+  // Show cards with regard to searchfield
+  return (
+    <div className="app-container">
+      <Header
+        onSearchChange={onSearchChange}
+        onCreateRelic={onCreateRelic}
         iconSize={iconSize}
         setIconSize={setIconSize}
-			/>
-			<Scroll>
+      />
+      <Scroll>
         <CardList
           userid={userid}
           relics={filteredRelics}
           iconSize={iconSize}
         />
-			</Scroll>
-		</div>
-	)
+      </Scroll>
+    </div>
+  )
 }
 
 export default App
